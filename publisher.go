58205a883a0a0371cf9e689f9cd88f9e607bdf17
Aloïs Micard <lunamicard@gmail.com>
2019-08-02 06:53:58 +0200

    Start implementing publisher

--- publisher.go
+++ publisher.go
@@ -3,3 +3,25 @@ package amqp
 type Publisher interface {
 	Publish(exchange, key string, message []byte) error
 }
+
+type StateLessPublisher struct {
+}
+
+type StateFullPublisher struct {
+}
+
+func NewStateLessPublisher(uri string) (Publisher, error) {
+	return nil, nil
+}
+
+func NewStateFullPublisher(uri string) (Publisher, error) {
+	return nil, nil
+}
+
+func (slp *StateLessPublisher) Publish(exchange, key string, message []byte) error {
+	return nil
+}
+
+func (sfp *StateFullPublisher) Publish(exchange, key string, message []byte) error {
+	return nil
+}
