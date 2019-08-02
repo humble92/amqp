50895177f9fc66c084d9ef441561798e60b8ac83
Aloïs Micard <lunamicard@gmail.com>
2019-08-02 06:51:02 +0200

    Separate consumer from publisher

---
+++ consumer.go
@@ -0,0 +1,4 @@
+package amqp
+
+type Consumer interface {
+}
--- publisher.go
+++ publisher.go
@@ -3,6 +3,3 @@ package amqp
 type Publisher interface {
 	Publish(exchange, key string, message []byte) error
 }
-
-type Consumer interface {
-}
