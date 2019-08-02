b132ae0c72ef2a306d8fccddd1e7df0e483c5de7
Aloïs Micard <lunamicard@gmail.com>
2019-08-02 06:54:41 +0200

    Fix visibility of internal struct

--- publisher.go
+++ publisher.go
@@ -4,10 +4,10 @@ type Publisher interface {
 	Publish(exchange, key string, message []byte) error
 }
 
-type StateLessPublisher struct {
+type stateLessPublisher struct {
 }
 
-type StateFullPublisher struct {
+type stateFullPublisher struct {
 }
 
 func NewStateLessPublisher(uri string) (Publisher, error) {
@@ -18,10 +18,10 @@ func NewStateFullPublisher(uri string) (Publisher, error) {
 	return nil, nil
 }
 
-func (slp *StateLessPublisher) Publish(exchange, key string, message []byte) error {
+func (slp *stateLessPublisher) Publish(exchange, key string, message []byte) error {
 	return nil
 }
 
-func (sfp *StateFullPublisher) Publish(exchange, key string, message []byte) error {
+func (sfp *stateFullPublisher) Publish(exchange, key string, message []byte) error {
 	return nil
 }
